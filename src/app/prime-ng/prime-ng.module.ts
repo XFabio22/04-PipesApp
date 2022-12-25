35ea1372994a74c91e95d42ac251f0c727075068
XFabio22 <98523846+XFabio22@users.noreply.github.com>
2022-12-25 21:04:05 +0100

    prime toolbar y prime table

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -4,6 +4,8 @@ import {ButtonModule} from 'primeng/button';
 import {CardModule} from 'primeng/card';
 import {FieldsetModule} from 'primeng/fieldset';
 import {MenubarModule} from 'primeng/menubar';
+import {ToolbarModule} from 'primeng/toolbar';
+import {TableModule} from 'primeng/table';
 
 
 
@@ -13,6 +15,8 @@ import {MenubarModule} from 'primeng/menubar';
     CardModule,
     MenubarModule,
     FieldsetModule,
+    ToolbarModule,
+    TableModule,
   ]
 })
 export class PrimeNgModule { }
