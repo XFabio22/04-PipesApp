843bb4843e3c4bbf710dd2b91f5cb943f79055df
XFabio22 <98523846+XFabio22@users.noreply.github.com>
2022-12-25 21:13:47 +0100

    info prime table

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interface/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -7,7 +8,33 @@ import { Component } from '@angular/core';
 })
 export class OrdenarComponent {
   enMayusculas:boolean = true;
-
+  heroes : Heroe [ ] =[
+     {
+      nombre: 'superman',
+      vuela: true,
+      color:Color.azul
+     },
+     {
+      nombre: 'Batman',
+      vuela: false,
+      color:Color.negro
+     },
+     {
+      nombre: 'Robin',
+      vuela: false,
+      color:Color.verde
+     },
+     {
+      nombre: 'Daredevil',
+      vuela: false,
+      color:Color.rojo
+     },
+     {
+      nombre: 'Linterna verde',
+      vuela: true,
+      color:Color.verde
+     }
+  ]
   toggleMayusculas(){
     this.enMayusculas = !this.enMayusculas
   }
