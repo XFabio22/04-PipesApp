4e487e1190f82634a84d324cb38785395935aa89
XFabio22 <98523846+XFabio22@users.noreply.github.com>
2022-12-26 20:19:50 +0100

    Vuela pipe

--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -6,7 +6,7 @@ import { BasicosComponent } from './pages/basicos/basicos.component';
 import { OrdenarComponent } from './pages/ordenar/ordenar.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { MayusculasPipe } from './pipes/mayusculas.pipe';
-
+import { VuelaPipe } from './pipes/vuela.pipe';
 
 
 @NgModule({
@@ -15,7 +15,8 @@ import { MayusculasPipe } from './pipes/mayusculas.pipe';
     NoComunesComponent,
     BasicosComponent,
     OrdenarComponent,
-    MayusculasPipe
+    MayusculasPipe,
+    VuelaPipe
   ],
   exports:[
     NumerosComponent,
